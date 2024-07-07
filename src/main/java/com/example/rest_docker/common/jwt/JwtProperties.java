b83494002fb6635e57411796e9dc0a418f3d0783
chahyoungseok <xcohdsuts@gmail.com>
2024-07-07 22:04:33 +0900

    [RD-5][feature] : JWT 를 발급하는 서비스를 구현한다.

--- src/main/java/com/example/rest_docker/common/jwt/JwtProperties.java
+++ src/main/java/com/example/rest_docker/common/jwt/JwtProperties.java
@@ -7,10 +7,20 @@ public class JwtProperties {
     @Value("${yml 에서 가져와야하는 경로}")
     public static String SECRET_KEY;
 
-    public static int EXPIRATION_TIME = 864000000; // 10일 (1/1000초)
+    public static final int ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 6; // 6시간 (1/1000초)
 
-    public static String TOKEN_PREFIX = "Bearer ";
+    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 10; // 10일 (1/1000초)
 
-    public static String HEADER_STRING = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    public static final String HEADER_STRING = "Authorization";
+
+    public static final String TYPE = "typ";
+
+    public static final String JWT_TYPE = "JWT";
+
+    public static final String ALGORITHM = "alg";
+
+    public static final String HMAC512 = "HMAC512";
 
 }
