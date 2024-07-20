4b178a923c6aa285b1a330e328850301c1c9e6e0
chahyeongseog <hscha@autocrypt.io>
2024-07-20 18:08:24 +0900

    [RD-9][feature] Auditor 와 History Entity 구현

---
+++ src/main/java/com/example/rest_docker/account/domain/entity/AccountLogoutHistoryEntity.java
@@ -0,0 +1,34 @@
+package com.example.rest_docker.account.domain.entity;
+
+import com.example.rest_docker.common.domain.BaseDomainHistoryEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "account_logout_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AccountLogoutHistoryEntity extends BaseDomainHistoryEntity {
+
+    @Column(name = "failure", nullable = false)
+    private Boolean failure;
+
+    @Column(name = "failure_reason")
+    private String failureReason;
+
+    @Column(name = "logout_method")
+    private String logoutMethod;
+
+    @Builder
+    public AccountLogoutHistoryEntity(String ipAddress, Boolean failure, String failureReason, String logoutMethod) {
+        super(ipAddress);
+        this.failure = failure;
+        this.failureReason = failureReason;
+        this.logoutMethod = logoutMethod;
+    }
+}
