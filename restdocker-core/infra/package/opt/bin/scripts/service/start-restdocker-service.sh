2c93509b8ba1bd8decb871fda0f69765a445d9e4
chahyoungseok <xcohdsuts@gmail.com>
2024-09-05 14:54:31 +0900

    [RD-24][refactor] 메모리를 차지하는 DockerNetwork 를 새로 생성하기보단 기존에 있는 bridge 네트워크 사용

--- restdocker-core/infra/package/opt/bin/scripts/service/start-restdocker-service.sh
+++ restdocker-core/infra/package/opt/bin/scripts/service/start-restdocker-service.sh
@@ -11,8 +11,6 @@ fi
 docker run --name ${RESTDOCKER_CONTAINER_NAME} \
 	   --rm \
 	   -itd \
-	   --net ${RESTDOCKER_DOCKER_NETWORK_NAME} \
-	   --ip ${RESTDOCKER_DOCKER_NETWORK_IP} \
 	   -p 443:8080 \
 	   -e SPRING_PROFILES_ACTIVE=prod \
 	   -e TZ=Asia/Seoul \
