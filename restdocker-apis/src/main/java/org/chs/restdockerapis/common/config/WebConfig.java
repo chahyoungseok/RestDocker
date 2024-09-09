8629476fcf3c9dd754ecccd42f5f305e77ecc0a5
chahyoungseok <xcohdsuts@gmail.com>
2024-09-09 16:57:43 +0900

    [RD-28][Bug] 정적리소스 제공 핸들러에 정의

--- restdocker-apis/src/main/java/org/chs/restdockerapis/common/config/WebConfig.java
+++ restdocker-apis/src/main/java/org/chs/restdockerapis/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package org.chs.restdockerapis.common.config;
 import org.chs.restdockerapis.common.argument_resolver.RequesterArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new RequesterArgumentResolver());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("classpath:/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
